af26cbd7aaa90b42f13e8db32c99bab51a6964fc
Dipayan Sarkar <dipayansarkar26@gmail.com>
2025-11-20 22:21:22 +0530

    in train 1

--- train.py
+++ train.py
@@ -22,8 +22,8 @@ if __name__ == "__main__":
         num_experts=32,
         experts_per_token=4,
         num_hidden_layers=4,
-        hidden_size=512,
-        intermediate_size=512
+        hidden_size=256,
+        intermediate_size=256
     ), device)
 
     print(sum([p.numel() for p in model.parameters()]) / 1000000, "M parameters")
